92c0e2053c44835fff786f2fc74e427be4883f5a
Chiryasova Ekaterina <kat.rok@mail.ru>
2024-08-16 13:53:53 +0300

    Добавлены файлы

---
+++ HotDrink1/VendingMachine.java
@@ -0,0 +1,12 @@
+package HotDrink1;
+
+import java.util.List;
+
+ public interface VendingMachine {
+     void initProducts (List <Product> list);
+    public Product getProduct(String name);
+
+
+
+
+}
